a1db8b469808f342c96fabbf95ae1121b80d419a
Michael Omale <michaelomaleson@gmail.com>
2026-05-03 13:43:43 +0100

    feat: push core source files

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useRoute } from 'vue-router'
+import { RouterView } from 'vue-router'
+import SiteFooter from './components/SiteFooter.vue'
+import SiteNavbar from './components/SiteNavbar.vue'
+
+const route = useRoute()
+</script>
+
+<template>
+  <div class="min-h-screen bg-background text-on-background texture-bg">
+    <SiteNavbar v-if="!route.meta.hideShell" />
+    <RouterView />
+    <SiteFooter v-if="!route.meta.hideShell" />
+  </div>
+</template>
